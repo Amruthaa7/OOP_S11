4acf3c472feb65561b50e41b5867e336ade7a864
Amruthaa7 <mandiga.amrutha@gmail.com>
2025-08-01 10:22:07 +0530

    Add files via upload

---
+++ PRACTICAL1/StringExample.java
@@ -0,0 +1,9 @@
+package Examples;
+
+public class StringExample {
+ 
+	public static void main(String[] args) {
+        String greeting = "Hello, World!";
+        System.out.println(greeting);
+    }
+}
